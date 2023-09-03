eca261d6d5396e25d50f68717497d8d95749ee9f
Albert Hoxha <123065766+alberthoxha@users.noreply.github.com>
2023-09-03 23:42:20 +0200

    card design

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3 @click="toogleComplete">{{ project.title }} </h3>
+    </div>
+    <div class="details">
+      <p v-if="showDetails">{{ project.details }}</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['project'],
+  data() {
+    return {
+      showDetails: false
+    }
+  },
+  methods: {
+    toogleComplete() {
+      this.showDetails = !this.showDetails
+    }
+  },
+}
+</script>
+
+<style >
+.project {
+  margin: 20px auto;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 8px;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+  border-left: 4px solid #e90054;
+
+}
+
+h3 {
+  cursor: pointer;
+}
+</style>
