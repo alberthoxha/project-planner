58f149625b7800d058119e051cdd1f6a6df8ecfa
Albert Hoxha <123065766+alberthoxha@users.noreply.github.com>
2023-09-03 22:28:20 +0200

    fetching data

--- src/views/About.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <h1>About</h1>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at blanditiis delectus dicta distinctio dolore et ex, facere laboriosam minima nemo neque officiis quibusdam quidem, recusandae repellendus sed sint veritatis voluptas voluptate?</p>
-</template>
