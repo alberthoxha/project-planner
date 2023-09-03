0f6269226c740c5d768ab0f83dbb2e00c2d89f6f
Albert Hoxha <123065766+alberthoxha@users.noreply.github.com>
2023-09-03 21:43:44 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Home Page</h1>
+ <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum iure minima quam similique voluptatum. Corporis eligendi fugiat harum labore libero, maiores pariatur quia repellat, repellendus similique soluta tenetur totam voluptate voluptatibus voluptatum.</p>
+</template>
+<script setup>
+</script>
---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>Job Details</h2>
+  <p>The Job id is {{id}}</p>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  // data() {
+  //   return {
+  //     id: this.$route.params.id
+  //   }
+  // }
+}
+</script>
